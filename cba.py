9db89ec66ad1338d6e57f6449d0676f695660939
Pranshu Gupta <pranshug@iitk.ac.in>
2016-03-05 15:42:20 +0530

    Updated README

--- cba.py
+++ cba.py
@@ -1,4 +1,21 @@
+#!/usr/bin/python3
+# =============================================================================
 # Crowd Behavior Ananlysis
 # Author: Pranshu Gupta, Lavisha Aggarwal
 # =============================================================================
 
+import numpy as np
+import scipy
+import cv2
+
+# -----------------------------------------------------------------------------
+# Hamming Distance
+def hamming(u, v):
+	"""hamming : computes the hamming distance between two bit-vectors"""
+	return scipy.spatial.distance.hamming(u, v)
+
+# Social Affinity Map
+def sam():
+	"""sam : computes the social affinity map for a given tracklet"""
+	
+# -----------------------------------------------------------------------------
