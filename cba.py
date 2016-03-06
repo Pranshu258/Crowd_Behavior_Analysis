7f4ae7244e5c5c8ebc3b344d52fec72283f71a52
Pranshu Gupta <pranshug@iitk.ac.in>
2016-03-06 18:39:53 +0530

    Added code to read CSV data from file

--- cba.py
+++ cba.py
@@ -5,8 +5,10 @@
 # =============================================================================
 
 import scipy
+import csv
 
 # -----------------------------------------------------------------------------
+
 # Hamming Distance
 def hamming(u, v):
 	"""hamming : computes the hamming distance between two bit-vectors"""
@@ -15,5 +17,13 @@ def hamming(u, v):
 # Social Affinity Map
 def sam():
 	"""sam : computes the social affinity map for a given tracklet"""
+
+# CSV Reader
+def getData(filename):
+	DATA = []
+	csvfile = open(filename, newline='')
+	data = csv.reader(csvfile, delimiter=' ', quotechar='|')
+	for row in data:
+		print(', '.join(row).split(";"))
 
 # -----------------------------------------------------------------------------
