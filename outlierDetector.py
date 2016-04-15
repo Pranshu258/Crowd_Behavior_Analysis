375774058efb7d6264516019c750b046e1eb9d61
Pranshu Gupta <pranshug@iitk.ac.in>
2016-04-15 19:34:20 +0530

    Added the speed component in tp_distance, corrected the density value, @lavisha

--- outlierDetector.py
+++ outlierDetector.py
@@ -23,18 +23,30 @@ from copy import deepcopy
 # METHODS
 
 # This method returns the standard deviation of the elements in a list
-def standard_deviation(l):
-	return np.std(np.array(l))
-
+#def standard_deviation(l):
+#   return np.std(np.array(l))
+def standard_deviation(distances):
+    #for Li in L:
+    #    dist=length(Li[0])
+    #    distances.append(dist)
+    # for d in distances:
+    #     if not math.isnan(d):
+    #         print(d)
+    std = np.std(np.array(distances))
+    print ("Mean Distance: ",np.mean(np.array(distances)) )
+    print ("Minimum Distance: ",np.min(np.array(distances)) )
+    print ("Maximum Distance: ",np.max(np.array(distances)) )
+    print ("Standard Deviation of Distances: ", std)  
+    return std
 
 # Distance between two t-partitions
 # This is not the distance measure used in the paper by Lee et. al.
 # def tp_distance(L1, L2):
-# 	# print("L1 = ", L1, "L2 = ", L2)
-# 	s1x, s1y, e1x, e1y = L1[0][0], L1[0][1], L1[1][0], L1[1][1]
-# 	s2x, s2y, e2x, e2y = L2[0][0], L2[0][1], L2[1][0], L2[1][1]
-# 	D = np.linalg.norm([s2y-s1y, s2x-s1x]) + np.linalg.norm([e2y-e1y, e2x-e1x])
-# 	return D
+#   # print("L1 = ", L1, "L2 = ", L2)
+#   s1x, s1y, e1x, e1y = L1[0][0], L1[0][1], L1[1][0], L1[1][1]
+#   s2x, s2y, e2x, e2y = L2[0][0], L2[0][1], L2[1][0], L2[1][1]
+#   D = np.linalg.norm([s2y-s1y, s2x-s1x]) + np.linalg.norm([e2y-e1y, e2x-e1x])
+#   return D
 
 # This function returns the projection point P2 of a point P1 on a line L
 def projPointOnLine(point, line_2pt):
@@ -65,8 +77,10 @@ def projPointOnLine(point, line_2pt):
 #This method determines the all the three distances between L1 and L2  
 def tp_distance(L1, L2):
 
-    w1, w2, w3 = 1.0, 1.0, 1.0
-
+    w1, w2, w3, w4 = 1.0, 1.0, 1.0, 1.0
+    #Computing Speed Distance
+    Dspeed=abs( (length(L1)/L1[0][2])-(length(L2)/L2[0][2]) )
+    #print(Dspeed)
     if length(L1) > length(L2):
         temp = deepcopy(L2)
         L2 = deepcopy(L1)
@@ -103,9 +117,9 @@ def tp_distance(L1, L2):
     costheta = inner_product/(np.linalg.norm([x1, y1])*np.linalg.norm([x2, y2]))
     #print(x1, y1, x2, y2, inner_product, (np.linalg.norm([x1, y1])*np.linalg.norm([x2, y2])), costheta)
     if costheta < -1:
-    	costheta = -1
+        costheta = -1
     elif costheta > 1:
-    	costheta = 1
+        costheta = 1
     angle = math.acos(costheta)
 
     if angle < (math.pi/2):
@@ -113,13 +127,13 @@ def tp_distance(L1, L2):
     else:
         Dang = length(L2) 
 
-    return w1*Dper + w2*Dpar + w3*Dang
+    return w1*Dper + w2*Dpar + w3*Dang + w4*Dspeed
  
 
 # This method returns the length of a t-partition
 def length(segment):
-	sx, sy, ex, ey = segment[0][0], segment[0][1], segment[1][0], segment[1][1]
-	return np.linalg.norm([ex-sx, ey-sy])
+    sx, sy, ex, ey = segment[0][0], segment[0][1], segment[1][0], segment[1][1]
+    return np.linalg.norm([ex-sx, ey-sy])
 
 # Minimum Description Length Cost with partitioning
 def mdl_par(t, s, e):
@@ -164,82 +178,110 @@ def mdl_partition(t):
 
 
 def partition(T):
-	L = []
-	for p in T:
-		#T[p] = partition(T[p])
-		for i in range(len(T[p])-1):
-			segment = [T[p][i], T[p][i+1]]
-			L.append([segment,p,0])
-	return L
+    L = []
+    for p in T:
+        #T[p] = partition(T[p])
+        for i in range(len(T[p])-1):
+            segment = [T[p][i], T[p][i+1]]
+            L.append([segment,p,0])
+    return L
+    
+def TotalDensity(T, L, D):
+    distances = []
+    for Li in L:
+        #distances = []
+        CTR_count = 0
+        for p in T:
+            if p != Li[1]:
+                matchLen = 0
+                for i in range(len(T[p])-1):
+                    segment = [T[p][i], T[p][i+1]]
+                    #print(segment, p)
+                    dist = tp_distance(Li[0], segment)
+                    if not math.isnan(dist):
+                        distances.append(dist)
+    sd = standard_deviation(distances)
+    tdensity=0
+    for Li in L:
+        distances = []
+        for p in T:
+            if p != Li[1]:
+                for i in range(len(T[p])-1):
+                    segment = [T[p][i], T[p][i+1]]
+                    #print(segment, p)
+                    dist = tp_distance(Li[0], segment)
+                    distances.append(dist)
+                tdensity = tdensity+(len([d for d in distances if d <= sd]) + 1)
+    return tdensity , sd
 
 def detect(T, L, D, P):
-	outlier_count = 0
-	c = 0
-	for Li in L:
-		distances = []
-		CTR_count = 0
-		for p in T:
-			if p != Li[1]:
-				matchLen = 0
-				for i in range(len(T[p])-1):
-					segment = [T[p][i], T[p][i+1]]
-					#print(segment, p)
-					dist = tp_distance(Li[0], segment)
-					distances.append(dist)
-					if dist < D:
-						matchLen = matchLen + length(segment)
-				if matchLen > length(Li[0]):
-					CTR_count = CTR_count + 1
-		# Calculate the density
-		sd = standard_deviation(distances)
-		density = (len([d for d in distances if d <= sd]) + 1)/len(L)
-		# print(CTR_count, density, CTR_count/density, P*len(T))
-		if CTR_count/density < P*len(T):
-			Li[2] = 1
-			outlier_count += 1
-			# print("Outlying Segment Found: ", Li)
-		c = c + 1
-		# print(np.mean(distances))
-		# print(c)
-	return L, outlier_count
+    outlier_count = 0
+    c = 0
+    totaldensity , sd  = TotalDensity(T,L,D)
+    for Li in L:
+        distances = []
+        CTR_count = 0
+        for p in T:
+            if p != Li[1]:
+                matchLen = 0
+                for i in range(len(T[p])-1):
+                    segment = [T[p][i], T[p][i+1]]
+                    #print(segment, p)
+                    dist = tp_distance(Li[0], segment)
+                    distances.append(dist)
+                    if dist < D:
+                        matchLen = matchLen + length(segment)
+                if matchLen > length(Li[0]):
+                    CTR_count = CTR_count + 1
+        # Calculate the density
+        density = (len([d for d in distances if d <= sd]) + 1)*len(L)
+        # print(CTR_count, density, CTR_count/density, P*len(T))
+        if (CTR_count*totaldensity)/density < P*len(T):
+            Li[2] = 1
+            outlier_count += 1
+            # print("Outlying Segment Found: ", Li)
+        c = c + 1
+        # print(np.mean(distances))
+        # print(c)
+    return L, outlier_count
 
 def mark(T, L, F):
-	otraj = []
-	for p in T:
-		# If the ratio of length of outlying t-partitions to total length of trajectory is greater than F
-		# Then mark this trajectory as outlying
-		outliers = [Li[0] for Li in L if (Li[2] == 1 and Li[1] == p)]
-		olen, tlen = 0, 0
-		for seg in outliers:
-			olen = olen + length(seg)
-		for i in range(len(T[p])-1):
-			segment = [T[p][i], T[p][i+1]]
-			tlen = tlen + length(segment)
-		if olen/tlen > F:
-			otraj.append(p)
-		# print(p)
-	return otraj
+    otraj = []
+    for p in T:
+        # If the ratio of length of outlying t-partitions to total length of trajectory is greater than F
+        # Then mark this trajectory as outlying
+        outliers = [Li[0] for Li in L if (Li[2] == 1 and Li[1] == p)]
+        olen, tlen = 0, 0
+        for seg in outliers:
+            olen = olen + length(seg)
+        for i in range(len(T[p])-1):
+            segment = [T[p][i], T[p][i+1]]
+            tlen = tlen + length(segment)
+        if olen/tlen > F:
+            otraj.append(p)
+        # print(p)
+    return otraj
 
 # The outlier detection algorithm
 def traod(T, D, P, F):
-	# PARTITION PHASE
-	# Partition each trajectory and store the t-partitions in array L
-	print("Partition Phase Begins ...")
-	L = partition(T)
-	print("Partition Done !")
-	print("Total Number of t-partitions: ", len(L))
-	print("Outlying t-partition Detection Phase Begins ...")
-	# DETECTION PHASE
-	# For each t-partition in L count the number of trajectories that are close to it
-	L, outlier_count = detect(T, L, D, P)
-	print("Outlying t-partition Detection Done !")
-	print("Number of Outlying t-partitions: ", outlier_count, " of ", len(L))
-	print("Outlying Trajectory Detection Phase Begins ...")
-	# MARKING PHASE
-	otraj = mark(T, L, F)
-	print("Outlying Trajectory Detection Phase Done !")
-	print("Number of Outlying Trajectories: ", len(otraj), " of ", len(T))
-	return otraj
+    # PARTITION PHASE
+    # Partition each trajectory and store the t-partitions in array L
+    print("Partition Phase Begins ...")
+    L = partition(T)
+    print("Partition Done !")
+    print("Total Number of t-partitions: ", len(L))
+    print("Outlying t-partition Detection Phase Begins ...")
+    # DETECTION PHASE
+    # For each t-partition in L count the number of trajectories that are close to it
+    L, outlier_count = detect(T, L, D, P)
+    print("Outlying t-partition Detection Done !")
+    print("Number of Outlying t-partitions: ", outlier_count, " of ", len(L))
+    print("Outlying Trajectory Detection Phase Begins ...")
+    # MARKING PHASE
+    otraj = mark(T, L, F)
+    print("Outlying Trajectory Detection Phase Done !")
+    print("Number of Outlying Trajectories: ", len(otraj), " of ", len(T))
+    return otraj
 
 
 ###################################################################################################
@@ -268,12 +310,12 @@ def trajectory(filename, N):
         t2, m2, x2, y2, p2 = gettime((r[0].split('T')[1])[:-4]), r[1], int(int(r[2])/67), int(int(r[3])/67), int(r[4])
         if (p1 == p2 and t1 != t2):
             if trajectory[P][len(trajectory[P])-1][0] != x2 or trajectory[P][len(trajectory[P])-1][1] != y2:
-                trajectory[P].append([x2,y2])
+                trajectory[P].append([x2,y2,t2])
         if p1 != p2:
             P = P + 1
             if not P <= N:
                 break
-            trajectory[P] = [[x2,y2]]
+            trajectory[P] = [[x2,y2,t2]]
         t1, m1, p1, x1, y1 = t2, m2, p2, x2, y2
     return trajectory
 
@@ -288,26 +330,27 @@ def plot_trajectory(traj, p):
     fig = plt.figure()
     # ax = fig.add_subplot(111, projection='3d')
     # ax.plot(x,y,t)
-    # plt.show()
-    plt.plot(x, y)
-    # plt.show()
+    #plt.show()
+    #plt.plot(x, y)
+    plt.show()
     fig.suptitle('TRAJECTORY ID: ' + str(p))
     plt.xlabel('x')
     plt.ylabel('y')
     fig.savefig(str(p) + '.png')
 # Synthetic data generation method
 # def addbad_trajectories(T):
-# 	i = len(T)+1
-# 	return T
+#   i = len(T)+1
+#   return T
 
 
 ###################################################################################################
 # EXECUTION SECTION
 
 T = trajectory(filename, 10)
-O = traod(T, 20, 0.01, 0.4)
+O = traod(T, 40, 0.01, 0.4)
 print("Outliers: ", O)
 ###################################################################################################
 # RESULTS PLOTTING SECTION
 for p in T:
-	plot_trajectory(T[p], p)
+    plot_trajectory(T[p], p)
+
